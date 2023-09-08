4064c2ce27b0a125687bdc8ec6e2550745ac746e
Gleidson G. de Oliveira dos Santos <gleidsonguilhermecpd@hotmail.com>
2023-09-08 15:59:13 -0300

    Injeção de independencia

---
+++ Dastone.CrossCutting/InversionOfControl/AutoMapperConfig/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using Dastone.Domain.Entity;
+using Dastone.ViewModel.Entity;
+
+namespace Dastone.CrossCutting.InversionOfControl.AutoMapperConfig
+{
+    public class MappingProfile : AutoMapper.Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Client, ClientViewModel>();
+            CreateMap<ClientViewModel, Client>();
+        }
+    }
+}
--- Dastone.Web/Program.cs
+++ Dastone.Web/Program.cs
@@ -1,7 +1,17 @@
+using Dastone.CrossCutting.InversionOfControl.Service;
+using Dastone.CrossCutting.InversionOfControl.AuctionManagerDbConfig;
+using Dastone.CrossCutting.InversionOfControl.AutoMapperConfig;
+using Dastone.CrossCutting.InversionOfControl.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddMySqlDependency(builder.Configuration);
+builder.Services.AddMySqlRepositoryDependency();
+builder.Services.AddServiceDependency();
+builder.Services.AddMapperConfiguration();
 
 var app = builder.Build();
 
